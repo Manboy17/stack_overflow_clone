a3131395908c16fb2e80d91a3bb32f3fc03047d0
denys <denia15042005@gmail.com>
2023-11-09 13:06:53 +0100

    create badge system

--- components/shared/Stats.tsx
+++ components/shared/Stats.tsx
@@ -1,4 +1,6 @@
 import { formatAndDivideNumber } from "@/lib/utils";
+import { BadgeCounts } from "@/types";
+
 import Image from "next/image";
 import React from "react";
 
@@ -25,12 +27,16 @@ const BadgeStage = ({ imageUrl, value, title }: BadgeStageProps) => {
 interface Props {
   totalQuestions: number;
   totalAnswers: number;
+  reputation: number;
+  badges: BadgeCounts;
 }
 
-const Stats = ({ totalQuestions, totalAnswers }: Props) => {
+const Stats = ({ totalQuestions, totalAnswers, reputation, badges }: Props) => {
   return (
     <div className="mt-10">
-      <h4 className="h3-semibold text-dark200_light900">Stats</h4>
+      <h4 className="h3-semibold text-dark200_light900">
+        Stats - {reputation}
+      </h4>
       <div className="mt-5 grid grid-cols-1 gap-5 xs:grid-cols-2 md:grid-cols-4">
         <div className="light-border background-light900_dark300 flex flex-wrap items-center justify-center gap-4 rounded-md border p-6 shadow-light-300 dark:shadow-dark-200">
           <div className="flex flex-col items-start">
@@ -49,17 +55,17 @@ const Stats = ({ totalQuestions, totalAnswers }: Props) => {
 
         <BadgeStage
           imageUrl="/assets/icons/gold-medal.svg"
-          value={0}
+          value={badges.GOLD}
           title="Gold Badges"
         />
         <BadgeStage
           imageUrl="/assets/icons/silver-medal.svg"
-          value={0}
+          value={badges.SILVER}
           title="Silver Badges"
         />
         <BadgeStage
           imageUrl="/assets/icons/bronze-medal.svg"
-          value={0}
+          value={badges.BRONZE}
           title="Bronze Badges"
         />
       </div>
